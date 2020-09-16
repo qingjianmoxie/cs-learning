cac849036dd1b4bee4199758e9c3743a2e10b37b
jie1993 <jiecfd@gmail.com>
2020-09-16 11:04:17 +0800

    Update 栈

--- DS&Algo/src/array_stack/array_stack.h
+++ DS&Algo/src/array_stack/array_stack.h
@@ -1,90 +1,79 @@
 /*
-*基于数组的栈实现
-*/
+ *基于数组的栈实现
+ */
 #ifndef ARRAY_STACK_HPP
 #define ARRAY_STACK_HPP
 
 template <typename T>
-class ArrayStack
-{
+class ArrayStack {
 public:
-	ArrayStack(int s = 10); //默认的栈容量为10
-	~ArrayStack();
+    ArrayStack(int s = 10);  //默认的栈容量为10
+    ~ArrayStack();
 
 public:
-	T top();		//获取栈顶元素
-	void push(T t); //压栈操作
-	T pop();		//弹栈操作
-	bool isEmpty(); //判空操作
-	int size();		//求栈的大小
+    T top();         //获取栈顶元素
+    void push(T t);  //压栈操作
+    T pop();         //弹栈操作
+    bool isEmpty();  //判空操作
+    int size();      //求栈的大小
 
 private:
-	int count;	  //栈的元素数量
-	int capacity; //栈的容量
-	T *array;	  //底层为数组
+    int count_;     //栈的元素数量
+    int capacity_;  //栈的容量
+    T *array_;      //底层为数组
 };
 
 /*构造函数*/
 template <typename T>
-ArrayStack<T>::ArrayStack(int s)
-	: count(0), capacity(s), array(nullptr)
-{
-	array = new T[capacity];
+ArrayStack<T>::ArrayStack(int s) : count_(0), capacity_(s), array_(nullptr) {
+    array_ = new T[capacity_];
 };
 
 /*析构函数*/
 template <typename T>
-ArrayStack<T>::~ArrayStack()
-{
-	if (array)
-	{
-		delete[] array;
-		array = nullptr;
-	}
+ArrayStack<T>::~ArrayStack() {
+    if (array_) {
+        delete[] array_;
+        array_ = nullptr;
+    }
 };
 
 /*栈的判空操作*/
 template <typename T>
-bool ArrayStack<T>::isEmpty()
-{
-	return count == 0; //栈元素为0时为栈空
+bool ArrayStack<T>::isEmpty() {
+    return count_ == 0;  //栈元素为0时为栈空
 };
 
 /*返回栈的大小*/
 template <typename T>
-int ArrayStack<T>::size()
-{
-	return count;
+int ArrayStack<T>::size() {
+    return count_;
 };
 
 /*插入元素*/
 template <typename T>
-void ArrayStack<T>::push(T t)
-{
-	if (count != capacity) //先判断是否栈满
-	{
-		array[count++] = t;
-	}
+void ArrayStack<T>::push(T t) {
+    if (count_ != capacity_)  //先判断是否栈满
+    {
+        array_[count_++] = t;
+    }
 };
 
 /*弹栈*/
 template <typename T>
-T ArrayStack<T>::pop()
-{
-	if (count != 0) //先判断是否是空栈
-	{
-		return array[--count];
-	}
+T ArrayStack<T>::pop() {
+    if (count_ != 0)  //先判断是否是空栈
+    {
+        return array_[--count_];
+    }
 };
 
 /*获取栈顶元素*/
 template <typename T>
-T ArrayStack<T>::top()
-{
-	if (count != 0)
-	{
-		return array[count - 1];
-	}
+T ArrayStack<T>::top() {
+    if (count_ != 0) {
+        return array_[count_ - 1];
+    }
 };
 
 #endif
--- DS&Algo/src/list_stack/list_stack.cc
+++ DS&Algo/src/list_stack/list_stack.cc
@@ -1,27 +1,26 @@
 /*
-*测试代码
-*/
+ *测试代码
+ */
+#include "list_stack.h"
+
 #include <iostream>
 #include <string>
-#include "list_stack.h"
 using namespace std;
 
-int main()
-{
-	LinkStack<string> lstack;
-	lstack.push("hello");
-	lstack.push("to");
-	lstack.push("you！");
+int main() {
+    LinkStack<string> lstack;
+    lstack.push("hello");
+    lstack.push("to");
+    lstack.push("you！");
 
-	cout << "栈的大小:" << lstack.size() << endl;
-	cout << "栈顶元素:" << lstack.top() << endl;
+    cout << "栈的大小:" << lstack.size() << endl;
+    cout << "栈顶元素:" << lstack.top() << endl;
 
-	while (!lstack.isEmpty())
-	{
-		lstack.pop();
-	}
+    while (!lstack.isEmpty()) {
+        lstack.pop();
+    }
 
-	cout << "栈的大小:" << lstack.size() << endl;
+    cout << "栈的大小:" << lstack.size() << endl;
 
-	return 0;
+    return 0;
 }
